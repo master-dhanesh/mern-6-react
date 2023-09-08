7ed350f7b9d586c71710cd63ef2d7e3ef9feec29
master-dhanesh <dhanesh1296@gmail.com>
2023-09-08 20:17:21 +0530

    Calling Api axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,64 @@
-import Create from "./components/Create";
-import Show from "./components/Show";
-import Home from "./components/Home";
-import { Link, Route, Routes } from "react-router-dom";
-import Info from "./components/Info";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
+    const [posts, setposts] = useState([]);
+    const [page, setpage] = useState(1);
+    const PrevHandler = () => {
+        if (page >= 1) {
+            setpage(page - 1);
+            GetPosts();
+        }
+    };
+    const NextHandler = () => {
+        setpage(page + 1);
+        GetPosts();
+    };
+
+    const GetPosts = async () => {
+        try {
+            const { data } = await axios.get(
+                `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+            );
+            setposts(data);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    let renderPosts = <h2>Loading...</h2>;
+    if (posts.length > 0) {
+        renderPosts = posts.map((post) => (
+            <div className="alert alert-light mb-3" key={post.id}>
+                <h4>{post.title}</h4>
+                <Link className="btn btn-dark" to={`/posts/${post.id}`}>
+                    {" "}
+                    Info
+                </Link>
+            </div>
+        ));
+    }
+
     return (
         <div className="container mt-5">
-            <ul className="list-group w-25">
-                <li className="list-group-item">
-                    <Link className="text-decoration-none" to="/">
-                        Home
-                    </Link>
-                </li>
-                <li className="list-group-item">
-                    <Link className="text-decoration-none" to="/create">
-                        Create
-                    </Link>
-                </li>
-                <li className="list-group-item">
-                    <Link className="text-decoration-none" to="/show">
-                        Show
-                    </Link>
-                </li>
-            </ul>
+            <h2>Get Posts</h2>
+            <button onClick={GetPosts} className="btn btn-primary">
+                Posts
+            </button>
             <hr />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/create" element={<Create />} />
-                <Route path="/show" element={<Show />}>
-                    <Route path="/show/:id" element={<Info />} />
-                </Route>
-            </Routes>
+            {renderPosts}
+
+            <hr className="my-5" />
+            <div className="text-center p-5">
+                <button onClick={PrevHandler} className="btn btn-warning me-4">
+                    Prev
+                </button>
+                <button onClick={NextHandler} className="btn btn-warning">
+                    Next
+                </button>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Context from "./utils/Context.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Context>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Context>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
