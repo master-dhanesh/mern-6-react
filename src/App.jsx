377e5ef02b1afb38c2c7451304fe4f8ca2a20038
master-dhanesh <dhanesh1296@gmail.com>
2023-09-11 20:23:12 +0530

    UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Show from "./Components/Show";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const App = () => {
     const [posts, setposts] = useState([]);
     const [page, setpage] = useState(1);
+    const [show, setshow] = useState(false);
+    const [hasMore, sethasMore] = useState(true);
     const PrevHandler = () => {
         if (page >= 1) {
             setpage(page - 1);
@@ -18,10 +22,15 @@ const App = () => {
 
     const GetPosts = async () => {
         try {
+            // const { data } = await axios.get(
+            //     `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+            // );
             const { data } = await axios.get(
-                `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+                `https://jsonplaceholder.typicode.com/posts?_limit=10&_start=${posts.length}`
             );
-            setposts(data);
+            data.length === 0 && sethasMore(false);
+            setposts([...posts, ...data]);
+            // setposts(data);
             console.log(data);
         } catch (error) {
             console.log(error);
@@ -41,16 +50,38 @@ const App = () => {
         ));
     }
 
+    useEffect(() => {
+        GetPosts();
+    }, []);
+
     return (
         <div className="container mt-5">
-            <h2>Get Posts</h2>
-            <button onClick={GetPosts} className="btn btn-primary">
-                Posts
+            <button className="btn btn-dark" onClick={() => setshow(!show)}>
+                {show ? "Hide" : "Show"}
             </button>
+            {show ? <Show /> : ""}
             <hr />
-            {renderPosts}
+            <h2>Get Posts</h2>
+            {/* <button onClick={GetPosts} className="btn btn-primary">
+                Posts
+            </button> */}
+            <hr />
 
-            <hr className="my-5" />
+            <InfiniteScroll
+                dataLength={posts.length} //This is important field to render the next data
+                next={GetPosts}
+                hasMore={hasMore}
+                loader={<h4 className="text-center p-5">Loading...</h4>}
+                endMessage={
+                    <p className="p-5" style={{ textAlign: "center" }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>
+                }
+            >
+                {renderPosts}
+            </InfiniteScroll>
+
+            {/* <hr className="my-5" />
             <div className="text-center p-5">
                 <button onClick={PrevHandler} className="btn btn-warning me-4">
                     Prev
@@ -58,7 +89,7 @@ const App = () => {
                 <button onClick={NextHandler} className="btn btn-warning">
                     Next
                 </button>
-            </div>
+            </div> */}
         </div>
     );
 };
